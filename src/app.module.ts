2c4412761b6aeb684e8055ee20ef9c94285fce46
fathallahhassen <hassenfathallah@gmail.com>
2026-04-02 21:41:37 +0100

    add Prometheus, grafana

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { BooksModule } from './books/books.module';
 import dbConfig from './config/db-config';
 import appConfig from './config/app-config';
 import { validate } from './config/config.type';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 
 @Module({
   imports: [
@@ -24,6 +25,12 @@ import { validate } from './config/config.type';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) =>
         configService.getOrThrow('database'),
+    }),
+    PrometheusModule.register({
+      path: '/metrics', // The endpoint where metrics will be exposed
+      defaultMetrics: {
+        enabled: true, // Collects default Node.js metrics (CPU, memory, etc.)
+      },
     }),
   ],
   controllers: [AppController, BooksController],
